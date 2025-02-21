717fe68ca81086613d6e31f9476f82bc658b953c
C.F.K <casper.kole@gmail.com>
2025-02-21 11:31:34 +0100

    fix: remove `test.py`

--- test.py
+++
@@ -1,7 +0,0 @@
-﻿import requests
-
-base_url = 'http://127.0.0.1:5000'
-xml_url = 'https://merapar-assessment-task.s3.eu-central-1.amazonaws.com/3dprinting-posts.xml'
-xml_url = 'https://merapar-assessment-task.s3.eu-central-1.amazonaws.com/arabic-posts.xml'
-response = requests.post(f'{base_url}/analyze', data={'url': xml_url})
-print(response.content)
